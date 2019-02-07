fee5d3afc654c4e57e7239b764dea48ea2b06c3d
Mathew Kleppin <matkle414@gmail.com>
2019-02-07 14:44:04 -0500

    use image resource post preview card

--- src/components/post/PostPreviewCard/index.js
+++ src/components/post/PostPreviewCard/index.js
@@ -5,26 +5,35 @@ import {
   PostPreviewImage,
   PostPreviewName,
 } from './styled';
+import { useImgResource } from '../../utils/useImgResource';
 
 const createLink = (id) => `/post/${id}`
 
-const PostPreviewCard = ({ className, onMouseOver, _id, name, source, image: { url } }) => (
-  <div className={className}>
-    <PostPreviewContainer>
-      <Link 
-        to={createLink(_id)}
-        onMouseOver={onMouseOver}
-      >
-        <PostPreviewImage
-          image={url}
-        />
-        <PostPreviewName>
-          <h3>{name}</h3>
-          <h6>{source}</h6>
-        </PostPreviewName>
-      </Link>
-    </PostPreviewContainer>
-  </div>
-)
+function PostPreviewCard({ className, onMouseOver, _id, name, source, image }) {
+  const imageUrl = useImgResource({
+    image,
+  }, 'med');
+
+  return (
+    <div className={className}>
+      <PostPreviewContainer>
+        <Link 
+          to={createLink(_id)}
+          onMouseOver={onMouseOver}
+        >
+          <PostPreviewImage
+            image={imageUrl}
+          />
+          <PostPreviewName>
+            <h3>{name}</h3>
+            <h6>{source}</h6>
+          </PostPreviewName>
+        </Link>
+      </PostPreviewContainer>
+    </div>
+  )
+}
+
+
 
 export default PostPreviewCard
